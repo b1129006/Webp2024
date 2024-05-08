b105ae1aed395d98ce91c6db127c4ff4f40844bd
b1129006 <120005503+b1129006@users.noreply.github.com>
2024-05-08 20:35:30 +0800

    Add files via upload

---
+++ hw3/react_datagrid/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import HW3 from './HW3.js';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ height: 0, marginBottom: 25 , fontFamily: '標楷體'}}>景點觀光展覽資訊</h1>
+      {
+        HW3()
+      }
+    </div>
+  );
+}
+
+export default App;
